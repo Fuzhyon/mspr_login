56229d0dee8e95bc0d53b87ba66ed071f5a3a2ec
jeffreyfevre <58734152+jeffreyfevre@users.noreply.github.com>
2021-07-14 13:36:36 +0200

    import ldap login et mvt

---
+++ settings.py
@@ -0,0 +1,16 @@
+
+from ldap3 import Server, Connection, ALL, SUBTREE
+from ldap3.core.exceptions import LDAPException, LDAPBindError
+
+# ldap server hostname and port
+ldsp_server = f"ldap://localhost:389"
+
+# dn
+root_dn = "dc=example,dc=org"
+
+# ldap user and password
+ldap_user_name = 'admin'
+ldap_password = 'admin'
+
+# user
+user = f'cn={ldap_user_name},root_dn'
