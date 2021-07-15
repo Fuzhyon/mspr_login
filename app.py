d169c2cdda7524a3b0cbac0e82ff272c18d6e6f9
jeffreyfevre <58734152+jeffreyfevre@users.noreply.github.com>
2021-07-15 22:34:14 +0200

    personne ne va regarder j'espère

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import pyotp
+from flask import *
+from flask_bootstrap import Bootstrap
 from flask import render_template, request
 from main import app
 from ldap3 import Server, Connection, ALL
 from forms.LoginForm import *
+
+app = Flask(__name__, template_folder='template')
+app.config["SECRET_KEY"] = "APP_SECRET_KEY"
+Bootstrap(app)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -19,13 +26,40 @@ def index():
         # validate the connection
         if login_msg == "Success":
             success_message = f"*** Authentication Success "
-            return render_template('success.html', success_message=success_message)
+            return redirect(url_for("login_2fa"))
 
         else:
             error_message = f"*** Authentication Failed - {login_msg}"
             return render_template("error.html", error_message=str(error_message))
 
     return render_template('login.html', form=form)
+
+
+# 2FA page route
+@app.route("/login/2fa/")
+def login_2fa():
+    # generating random secret key for authentication
+    secret = pyotp.random_base32()
+    return render_template("login_2fa.html", secret=secret)
+
+
+# 2FA form route
+@app.route("/login/2fa/", methods=['GET', 'POST'])
+def login_2fa_form():
+    # getting secret key used by user
+    secret = request.form.get("secret")
+    # getting OTP provided by user
+    otp = int(request.form.get("otp"))
+
+    # verifying submitted OTP with PyOTP
+    if pyotp.TOTP(secret).verify(otp):
+        # inform users if OTP is valid
+        flash("The TOTP 2FA token is valid", "success")
+        return redirect(url_for("login_2fa"))
+    else:
+        # inform users if OTP is invalid
+        flash("You have supplied an invalid 2FA token!", "danger")
+        return redirect(url_for("login_2fa"))
 
 
 if __name__ == '__main__':
